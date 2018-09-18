04c8c898d4cce60f06274d7e0968f88344932f35
Cameron Whitehead <camjw119@gmail.com>
2018-09-18 15:24:41 +0100

    Added missing files from commit

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require_relative 'lib/player'
 
 class Battle < Sinatra::Base
 
@@ -6,20 +7,32 @@ class Battle < Sinatra::Base
   set :session_secret, "nothing"
 
   get '/' do
-  erb(:index)
+    erb(:index)
   end
 
   post "/names" do
-    session[:Player_1]=params[:Player_1]
-    session[:Player_2]=params[:Player_2]
+    $Player_1 = Player.new(params[:Player_1])
+    $Player_2 = Player.new(params[:Player_2])
+    session[:Player_1_hp] = 600
+    session[:Player_2_hp] = 600
     redirect '/play'
   end
 
   get '/play' do
-    @Player_1 = session[:Player_1]
-    @Player_2 = session[:Player_2]
+    @Player_1 = $Player_1
+    @Player_2 = $Player_2
+    @Player_1_hp = session[:Player_1_hp]
+    @Player_2_hp = session[:Player_2_hp]
     erb(:play)
   end
 
+  post '/attack1' do
+    session[:Player_2_hp] -= 50
+    redirect '/play'
+  end
 
+  post '/attack2' do
+    session[:Player_1_hp] -= 50
+    redirect '/play'
+  end
 end
